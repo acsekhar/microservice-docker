907250ccd43b4b99ad3474710025bb109f570fe7
acsekhar <chandrasekhar.annavajjala@gmail.com>
2018-09-02 22:11:22 -0500

    Zuul changes

--- sample-zuul-server/src/main/java/org/meghraj/docker/samples/Application.java
+++ sample-zuul-server/src/main/java/org/meghraj/docker/samples/Application.java
@@ -1,16 +1,10 @@
 package org.meghraj.docker.samples;
 
-import org.meghraj.docker.samples.data.Account;
-import org.meghraj.docker.samples.jpa.AccountsRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by C840290 on 8/30/2018.
@@ -20,21 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @EnableAutoConfiguration
 @EnableConfigurationProperties
+
 @EnableEurekaClient
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-}
-
-@RestController
-class sample {
-    @Autowired
-    AccountsRepository accountsRepository;
-
-    @RequestMapping(value = "/sample", produces = "application/json")
-
-    public ResponseEntity<Account> hello() {
-        return ResponseEntity.ok(accountsRepository.findByUserName("user1"));
     }
 }
